e22c0827337105ab1033fd378bdc6677206b030b
Raghul KK <raghulkk@Raghuls-MacBook-Air.local>
2021-08-14 12:14:12 +0530

    changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,17 +3,16 @@ class BookingsController < ApplicationController
   skip_before_action :verify_authenticity_token  
 
 
+
   def create
-
-    @user = User.new
-
-    @car = Car.new
 
     @booking = Booking.create(user_id: params[:user_id], car_id: params[:car_id])
 
     render json: @booking.attributes 
 
   end
+
+
 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   def show
 
-    @user = User.find(params[:id]).bookings.cars
+    @user = User.find(params[:id]).bookings
     render json: @user
 
   end
